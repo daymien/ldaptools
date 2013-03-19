d5007154d681cf333528d105bb15d017c805e295
Reimund Klain <reimund@klain.eu>
2013-03-20 00:40:50 +0100

    argparse

--- trim.py
+++ trim.py
@@ -2,14 +2,23 @@ import ldap
 import sys
 import logging
 import pprint
+import argparse
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
 
 def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("uri", help="ldap URL")
+    parser.add_argument("user", help="user cn")
+    parser.add_argument("-p", "--password", help="user password")
+    
+    args = parser.parse_args()
+
     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
-    l = ldap.initialize("ldaps://example.org:636")
-    l.simple_bind_s("cn=admin,dc=example,dc=org", "password")
+    l = ldap.initialize(args.uri)
+    l.simple_bind_s(args.user, args.password)
 
     r = l.search_s("ou=contacts,dc=example,dc=org", ldap.SCOPE_SUBTREE)
     for dn, attrs in r:
